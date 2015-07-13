06a3de19e83fa07bd8a7103b4f23ee4a2202855e
Genevieve Shattow <genevieve.shattow@gmail.com>
2015-07-13 16:36:24 +0100

    took some if statements out, now requires grep before

--- vep_parser.py
+++ vep_parser.py
@@ -40,24 +40,20 @@ class ReadData :
 		tic = time.clock()
 		for item in file(datafile) :
 			ii = ii + 1
-			if item.startswith('##'):
-				continue
-			elif item.startswith('#') :
+			if item.startswith('#') :
 				print 'line number', ii, 'has the index names'
 				item = item.split()
-#				consequence_index = item.index('Consequence')
 				location_index = item.index('Location')
 				extra_index = item.index('Extra')
 			else :
 				item = item.split()
 				loc = item[location_index].split(':')
 				extra = item[extra_index].replace(';',' ').replace('=', ' ').split()
-				if set(['SYMBOL', 'IND', 'SIFT']) <= set(extra) :
-					if (extra[extra.index('SYMBOL_SOURCE') + 1] == params.catalogue) :
-						gene_index = extra.index('SYMBOL') + 1
-						individual_index = extra.index('IND')  + 1
-						genes_all.append(extra[gene_index])
-						individuals_all.append(extra[individual_index])
+				if (extra[extra.index('SYMBOL_SOURCE') + 1] == params.catalogue) :
+					gene_index = extra.index('SYMBOL') + 1
+					individual_index = extra.index('IND')  + 1
+					genes_all.append(extra[gene_index])
+					individuals_all.append(extra[individual_index])
 
 		toc = time.clock()	
 		print toc - tic, 'seconds to read in file, ', \
@@ -71,13 +67,10 @@ class ReadData :
 		print len(genes_all), 'unique genes'
 		print len(individuals_all), 'unique individuals'
 
+				
+		return individuals_all, genes_all, location_index, extra_index
 		
-#		print genes_all
-#		print individuals_all
-		
-		return individuals_all, genes_all
-		
-	def sort_vep_file(self, datafile, individuals_list, genes_list) :
+	def sort_vep_file(self, datafile, individuals_list, genes_list, location_index, extra_index) :
 		
 		n_g = len(genes_list)
 		n_p = len(individuals_list)
@@ -85,34 +78,23 @@ class ReadData :
 		
 		tic = time.clock()
 		for item in file(datafile) :
-			if item.startswith('##'):
-				continue
-			elif item.startswith('#') :
-				item = item.split()
-#				consequence_index = item.index('Consequence')
-				location_index = item.index('Location')
-				extra_index = item.index('Extra')
-			else :
+			if item.startswith('r') :
 				item = item.split()
 				loc = item[location_index].split(':')
 				extra = item[extra_index].replace(';',' ').replace('=', ' ').split()
-#				if item[consequence_index] != params.ignore_consequence :
-				if set(['SYMBOL', 'IND', 'SIFT']) <= set(extra) :
-					if (extra[extra.index('SYMBOL_SOURCE') + 1] == params.catalogue) :
-#							(extra[extra.index('IMPACT') + 1] == params.impact) :
-						sift = extra[extra.index('SIFT') + 1].replace('(', ' ').replace(')', ' ').split()
-						if float(sift[1]) < params.sift :
-							gene_index = extra.index('SYMBOL') + 1
-							individual_index = extra.index('IND')  + 1
-							ii_p = individuals_list.index(extra[individual_index])
-							ii_g = genes_list.index(extra[gene_index])
-							mutation_array[ii_p][ii_g] += 1
+				if (extra[extra.index('SYMBOL_SOURCE') + 1] == params.catalogue) :
+					sift = extra[extra.index('SIFT') + 1].replace('(', ' ').replace(')', ' ').split()
+					if float(sift[1]) < params.sift :
+						gene_index = extra.index('SYMBOL') + 1
+						individual_index = extra.index('IND')  + 1
+						ii_p = individuals_list.index(extra[individual_index])
+						ii_g = genes_list.index(extra[gene_index])
+						mutation_array[ii_p][ii_g] += 1
 
 		toc = time.clock()	
 		print toc - tic, 'seconds to read in file, ', \
 			float(len(genes_list))/(toc-tic), 'per second'		
 			
-#		print mutation_array
 		
 		return mutation_array
 
@@ -142,8 +124,10 @@ if __name__ == '__main__':
 	
 	datafile = params.datafile
 		
-	individuals_list, genes_list = rd.parse_vep_file(datafile)
-	mutation_array = rd.sort_vep_file(datafile, individuals_list, genes_list)
+	individuals_list, genes_list, location_index, extra_index = \
+		rd.parse_vep_file(datafile)
+	mutation_array = \
+		rd.sort_vep_file(datafile, individuals_list, genes_list, location_index, extra_index)
 
 
 	wt.write_mutation_array(mutation_array)
